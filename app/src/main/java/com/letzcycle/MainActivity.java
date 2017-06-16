8bf18761259a95577f3aee831c5ec81e46a1da11
im-ranu <ranuluhar@gmail.com>
2017-06-16 18:20:02 +0530

    Letzcycle

---
+++ app/src/main/java/com/letzcycle/MainActivity.java
@@ -0,0 +1,20 @@
+package com.letzcycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.mipmap.ic_menu_white_24dp);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/letzcycle/Splash.java
@@ -0,0 +1,48 @@
+package com.letzcycle;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Splash extends AppCompatActivity {
+
+    protected boolean _active = true;
+    protected int _splashTime = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+
+        Thread splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    while (_active && (waited < _splashTime)) {
+                        sleep(100);
+                        if (_active) {
+                            waited += 100;
+                        }
+                    }
+                } catch (Exception e) {
+
+                } finally {
+
+                    startActivity(new Intent(Splash.this,
+                            MainActivity.class));
+                    finish();
+                }
+            };
+        };
+        splashTread.start();
+    }
+    }
+
