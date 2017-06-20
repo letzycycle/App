05587500ec9c7066bb312e3db3097ba6d2e7a1eb
im-ranu <Ranu@123>
2017-06-20 16:22:43 +0530

    Letzcycle Android App

--- app/src/main/java/com/letzcycle/MainActivity.java
+++ app/src/main/java/com/letzcycle/MainActivity.java
@@ -1,128 +1,127 @@
 package com.letzcycle;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.location.Location;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.test.mock.MockPackageManager;
-import android.widget.Toast;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.UiSettings;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import static com.letzcycle.R.id.map;
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback,LocationProvider.LocationCallback {
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
-    private GoogleMap mMap;
-    private static final int REQUEST_CODE_PERMISSION = 2;
-    String mPermission = Manifest.permission.ACCESS_FINE_LOCATION;
-    double latitude, longitude;
+    public static final String TAG = MainActivity.class.getSimpleName();
 
-    // GPSTracker class
-    GPSTracker gps;
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    private LocationProvider mLocationProvider;
+    private ImageView imgMyLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         setContentView(R.layout.activity_main);
-
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-            return;
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{mPermission},
-                    REQUEST_CODE_PERMISSION);
-        }
-
-
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(map);
+                .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
 
+
+        mLocationProvider = new LocationProvider(this, this);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mLocationProvider.connect();
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_CODE_PERMISSION: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+    protected void onPause() {
+        super.onPause();
+        mLocationProvider.disconnect();
+    }
 
-                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
 
-                        mMap.setMyLocationEnabled(true);
-                    }
 
-                }
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+
+
+    public void handleNewLocation(Location location) {
+        Log.d(TAG, location.toString());
+
+        final double currentLatitude = location.getLatitude();
+        final double currentLongitude = location.getLongitude();
+        final LatLng latLng = new LatLng(currentLatitude, currentLongitude);
+        imgMyLocation = (ImageView) findViewById(R.id.imgMyLocation);
+        imgMyLocation.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                CameraPosition cameraPosition = new CameraPosition.Builder()
+                        .target(new LatLng(currentLatitude,currentLongitude))      // Sets the center of the map to location user
+                        .zoom(15.04f)                // Sets the orientation of the camera to east
+                        .tilt(40)                   // Sets the tilt of the camera to 30 degrees
+                        .build();                   // Creates a CameraPosition from the builder
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
             }
 
-        }
+        });
+
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(currentLatitude, currentLongitude)).title("Current Location"));
+        MarkerOptions options = new MarkerOptions()
+                .position(latLng)
+                .title("I am here!");
+        mMap.addMarker(options);
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(currentLatitude,currentLongitude))      // Sets the center of the map to location user
+                .zoom(15.04f)                // Sets the orientation of the camera to east
+                .tilt(40)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        Circle circle = mMap.addCircle(new CircleOptions()
+                .center(new LatLng(currentLatitude, currentLongitude))
+                .radius(500)
+                .strokeColor(0x5590b1e4).strokeWidth(2)
+                .fillColor(0x5590b1e4));
     }
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
         mMap = googleMap;
-
-        try {
-            if (ActivityCompat.checkSelfPermission(this, mPermission)
-                    != PackageManager.PERMISSION_GRANTED) {
-
-                ActivityCompat.requestPermissions(this, new String[]{mPermission},
-                        REQUEST_CODE_PERMISSION);
-
-
-            }
-            else
-            {
-                mMap.setMyLocationEnabled(true);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        // create class object
-        gps = new GPSTracker(MainActivity.this);
-
-        // check if GPS enabled
-        if(gps.canGetLocation()){
-
-            latitude = gps.getLatitude();
-            longitude = gps.getLongitude();
-
-        }else{
-            gps.showSettingsAlert();
-        }
-
-        UiSettings uiSettings = googleMap.getUiSettings();
-        uiSettings.setMapToolbarEnabled(false);
-        uiSettings.setZoomControlsEnabled(true);
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(latitude, longitude);
-
-        mMap.addMarker(new MarkerOptions().position(sydney).title("You are here..."));
-        Circle circle = mMap.addCircle(new CircleOptions()
-                .center(new LatLng(latitude, longitude))
-                .radius(1000)
-                .strokeColor(Color.BLUE).strokeWidth(2)
-                .fillColor(0x55ffff00));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude,longitude),14.04f));
 
 
     }
